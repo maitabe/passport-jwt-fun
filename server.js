b1f461183cb4d5695cac0b3586a4dcc59db0e062
maitabe <martushb@gmail.com>
2016-12-19 17:11:24 +0200

    done

--- server.js
+++ server.js
@@ -3,13 +3,35 @@ var app = express();
 var passport = require('passport');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+//decrypt the JWT
+var expressJWT = require('express-jwt');
+
+var auth = expressJWT({secret: 'SECRET'});
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 
 mongoose.connect('mongodb://localhost/passportjwt');
 
+var User = require('./UserModel');
+
 var LocalStrategy = require('passport-local').Strategy;
+
+//check if user exist or not on login
+passport.use('login', new LocalStrategy(function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+
+      return done(null, user);
+    });
+  }
+));
+
 
 app.get('/register', function (req, res) {
   res.sendFile(__dirname + '/register.html');
@@ -19,8 +41,8 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/hello', function (req, res) {
-  res.json({hello: 'world'});
+app.get('/hello', auth, function (req, res) {
+  res.json(req.user);
 });
 
 app.get('/login', function (req, res) {
@@ -29,10 +51,34 @@ app.get('/login', function (req, res) {
 
 app.post('/register', function(req, res){
 
+	var user = new User();
+
+	user.username = req.body.username;
+	user.password = req.body.password;
+
+	console.log(user);
+
+  user.save(function (err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()});
+  });
+
 });
 
-app.post('/login', function(req, res){
+app.post('/login', function(req, res, next){
+	passport.authenticate('login', function(err, user){
+    if(err){ return next(err); }
 
+    if (user) {
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401);
+    }
+  })(req, res, next);
 });
 
-app.listen(8000);
+
+app.listen(8000, function() {
+	console.log('started app, listening the radio too ');
+});
